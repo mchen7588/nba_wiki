3c2439665ecaced6b26466d9ccef7369871aede8
Michael Chen <mchen7588@gmail.com>
2019-04-03 20:00:11 -0700

    get rows and headers from nba wiki table

--- program.py
+++ program.py
@@ -22,11 +22,25 @@ def get_nba_wiki_table(html):
     return nba_wiki_table
 
 
+def get_rows(table_html):
+    rows = table_html[0].find_all('tr')
+    return rows
+
+
+def get_headers(rows):
+    headers = [th.text.strip() for th in rows[0].find_all('th')]
+    return headers
+
+
 def main():
     header()
     html = get_html()
     nba_wiki_table = get_nba_wiki_table(html)
-    print('table :', nba_wiki_table)
+    rows = get_rows(nba_wiki_table)
+    print('rows: ', len(rows))
+
+    headers = get_headers(rows)
+    print('headers: ', headers)
 
 
 if __name__ == '__main__':
