332093df2be3be20e9f8d51da111f9205c219c27
Michael Chen <mchen7588@gmail.com>
2019-04-05 22:29:28 -0700

    get conference and division

--- program.py
+++ program.py
@@ -27,9 +27,23 @@ def get_rows(table_html):
     return rows
 
 
-def get_headers(rows):
-    headers = [th.text.strip() for th in rows[0].find_all('th')]
+def get_headers(header_row):
+    headers = [get_text_from_html(th) for th in header_row.find_all('th')]
     return headers
+
+
+def get_conference(row):
+    conference = row.find('th', colspan=True)
+    print(get_text_from_html(conference))
+
+
+def get_division(row):
+    division = row.find('th', rowspan=True)
+    print(get_text_from_html(division))
+
+
+def get_text_from_html(html):
+    return html.text.strip() if html else None
 
 
 def main():
@@ -39,8 +53,18 @@ def main():
     rows = get_rows(nba_wiki_table)
     print('rows: ', len(rows))
 
-    headers = get_headers(rows)
+    headers = get_headers(rows[0])
     print('headers: ', headers)
+
+    get_conference(rows[1])  # East
+    get_conference(rows[17])  # West
+
+    get_division(rows[2])  # Atlantic
+    get_division(rows[7])  # Central
+    get_division(rows[12])  # Southeast
+    get_division(rows[18])  # Northwest
+    get_division(rows[23])  # Pacific
+    get_division(rows[28])  # Southwest
 
 
 if __name__ == '__main__':
